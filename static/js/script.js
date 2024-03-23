c56bba76fbda8b97b3291a641c801d00f20eb939
chxto1906 <chxto1906@gmail.com>
2024-03-23 02:13:02 -0500

    gunicorn socket

--- static/js/script.js
+++ static/js/script.js
@@ -7,7 +7,7 @@
             .then(function(stream) {
                 video.srcObject = stream;
                 video.play();
-                setInterval(captureAndSend, 100); // Iniciar la captura y envío de imágenes
+                setInterval(captureAndSend, 400); // Iniciar la captura y envío de imágenes
             })
             .catch(function(err) {
                 console.error('Error accessing webcam: ', err);
