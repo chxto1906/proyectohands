94699a33feee7f6ab123856c82e0352ad66cd31d
chxto1906 <chxto1906@gmail.com>
2024-03-23 00:25:08 -0500

    actualizando requirements y lint

--- app.py
+++ app.py
@@ -175,6 +175,7 @@ def handle_image(string_base64):
         with open("imagen.jpg", "wb") as f:
             f.write(image_data)
         frame_generator = generate_frames_img()
+        print('frame_generator', frame_generator)
         if frame_generator is not None:
             socketio.emit('frame', frame_generator)
     except Exception as e:
